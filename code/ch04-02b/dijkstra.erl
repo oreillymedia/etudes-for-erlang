56895716fe130dcdad07ec5f9ba121dfd47f4e55
J David Eisenberg <jdavid.eisenberg@gmail.com>
2013-06-15 11:24:15 -0700

    Added new solution to GCD, added note to contributor.

--- code/ch04-02b/dijkstra.erl
+++ code/ch04-02b/dijkstra.erl
@@ -13,10 +13,12 @@
 
 -spec(gcd(number(), number()) -> number()).
 
+gcd(M, N) when M == N ->
+  M;
+
+gcd(M,N) when M > N ->
+  gcd(M - N, N);
+
 gcd(M, N) ->
-  if
-    M == N  -> M;
-    M > N -> gcd(M - N, N);
-    true -> gcd(M, N - M)
-  end.
+  gcd(M, N - M).
 
